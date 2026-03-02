9901193f280f641fd26818a9880a95c911c91e94
folk23444 <Thanyaton.jv@gmail.com>
2026-03-02 19:01:39 +0700

    L

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,9 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { MissionController } from './mission/mission.controller';
-import { MissionService } from './mission/mission.service';
+import { Module } from "@nestjs/common";
+import { MissionModule } from "./mission/mission.module";
 
 @Module({
-  imports: [],
-  controllers: [AppController, MissionController],
-  providers: [AppService, MissionService],
+  imports: [MissionModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
