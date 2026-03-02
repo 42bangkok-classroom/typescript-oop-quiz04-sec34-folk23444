063aeb3b7761db006451cf98d75424273ac90e62
folk23444 <Thanyaton.jv@gmail.com>
2026-03-02 18:56:10 +0700

    L

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MissionController } from './mission/mission.controller';
+import { MissionService } from './mission/mission.service';
 
 @Module({
   imports: [],
-  controllers: [],
-  providers: [],
+  controllers: [AppController, MissionController],
+  providers: [AppService, MissionService],
 })
 export class AppModule {}
---
+++ src/mission/mission.service.ts
@@ -0,0 +1,49 @@
+/* eslint-disable */
+import { Injectable } from "@nestjs/common";
+import { IMission } from "./mission.interface.ts";
+import { readFileSync } from "fs";
+
+@Injectable()
+export class MissionService {
+  private readonly mission = [
+    { id: 1, codename: "OPERATION_STORM", status: "ACTIVE" },
+    { id: 2, codename: "SILENT_SNAKE", status: "COMPLETED" },
+    { id: 3, codename: "RED_DAWN", status: "FAILED" },
+    { id: 4, codename: "BLACKOUT", status: "ACTIVE" },
+    { id: 5, codename: "ECHO_FALLS", status: "COMPLETED" },
+    { id: 6, codename: "GHOST_RIDER", status: "COMPLETED" },
+  ];
+
+  getSummary() {
+    const res = {
+      ACTIVE: 0,
+      COMPLETED: 0,
+      FAILED: 0,
+    };
+    for (let i = 0; i < this.mission.length; i++) {
+      const data = this.mission[i];
+      if (data.status in res) {
+        res[data.status as keyof typeof res]++;
+      }
+    }
+    return res;
+  }
+
+  findAll() {
+    const data = readFileSync("missions.json", "utf-8");
+    const missions: IMission[] = JSON.parse(data);
+    missions.map(d=>{
+      let durationDays = -1
+      if (d.startDate && d.endDate) {
+        const start = new Date(d.startDate);
+        const end = new Date(d.endDate);
+        durationDays = Math.ceil((end.getTime() - start.getTime()) /86400000);
+      }
+      return {
+        ...d,
+        durationDays
+      }
+    })
+    return missions;
+  }
+}
